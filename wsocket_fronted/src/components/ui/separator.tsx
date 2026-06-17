d5aac56d28a6165c2a342a9ac3372deaa563dc95
ANUKOOL324 <anukoolbhul324@gmail.com>
2026-06-18 05:23:23 +0530

    add base separator and skeleton components

--- wsocket_fronted/src/components/ui/separator.tsx
+++ wsocket_fronted/src/components/ui/separator.tsx
@@ -1,7 +1,28 @@
-import type { HTMLAttributes } from 'react'
+"use client"
 
-import { cn } from '../../utils/cn'
+import * as React from "react"
+import { Separator as SeparatorPrimitive } from "radix-ui"
 
-export function Separator({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn('h-px w-full bg-white/8', className)} {...props} />
+import { cn } from "@/utils/index"
+
+function Separator({
+  className,
+  orientation = "horizontal",
+  decorative = true,
+  ...props
+}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+  return (
+    <SeparatorPrimitive.Root
+      data-slot="separator"
+      decorative={decorative}
+      orientation={orientation}
+      className={cn(
+        "shrink-0 bg-border data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
+        className
+      )}
+      {...props}
+    />
+  )
 }
+
+export { Separator }
--- wsocket_fronted/src/components/ui/skeleton.tsx
+++ wsocket_fronted/src/components/ui/skeleton.tsx
@@ -1,12 +1,13 @@
-import type { HTMLAttributes } from 'react'
+import { cn } from "@/utils/index"
 
-import { cn } from '../../utils/cn'
-
-export function Skeleton({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
+function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
-      className={cn('animate-pulse rounded-md bg-white/[0.06]', className)}
+      data-slot="skeleton"
+      className={cn("animate-pulse rounded-md bg-accent", className)}
       {...props}
     />
   )
 }
+
+export { Skeleton }
