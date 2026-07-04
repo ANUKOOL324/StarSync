2aa93fb7ecf0bf1f30385a1a4e2f0aceecab59b1
ANUKOOL <anukoolbhul324@gmail.com>
2026-07-04 17:24:26 +0530

    Move backend routes

--- Starsync_backend/src/routes/authRoutes.ts
+++ Starsync_backend/src/routes/authRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { login, me, signup } from "../controllers/authController";
+import { login, logout, me, signup } from "../controllers/authController";
 import { requireAuth } from "../middleware/authMiddleware";
 import { asyncHandler } from "../utils/asyncHandler";
 
@@ -8,4 +8,5 @@ export const authRoutes = Router();
 
 authRoutes.post("/signup", asyncHandler(signup));
 authRoutes.post("/login", asyncHandler(login));
+authRoutes.post("/logout", asyncHandler(logout));
 authRoutes.get("/me", requireAuth, asyncHandler(me));
