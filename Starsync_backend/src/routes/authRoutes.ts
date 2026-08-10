18337caebebe2c609e01b88fa7b87b7a37380930
ANUKOOL <anukoolbhul324@gmail.com>
2026-08-11 04:26:36 +0530

    rate limit authentication attempts

--- Starsync_backend/src/routes/authRoutes.ts
+++ Starsync_backend/src/routes/authRoutes.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 
 import { login, logout, me, signup } from "../controllers/authController";
 import { requireAuth } from "../middleware/authMiddleware";
+import { authRateLimiter } from "../middleware/rateLimiters";
 import { asyncHandler } from "../utils/asyncHandler";
 
 export const authRoutes = Router();
 
-authRoutes.post("/signup", asyncHandler(signup));
-authRoutes.post("/login", asyncHandler(login));
+authRoutes.post("/signup", authRateLimiter, asyncHandler(signup));
+authRoutes.post("/login", authRateLimiter, asyncHandler(login));
 authRoutes.post("/logout", asyncHandler(logout));
 authRoutes.get("/me", requireAuth, asyncHandler(me));
